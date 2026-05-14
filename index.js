584b602c070a3f89ffcbdbd61ce09e3eb7543839
RadtALIVE <radt.yt.dartt@gmail.com>
2026-05-14 22:03:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
+require('dotenv').config();
+const MTUwNDQ5MjkzOTc3MzA4Nzc2NA.G-TSUp.uPI6p9BIO5kEfDH_dgjnXyo3L3m5wUEAZLpzpg
 
+ = process.env.TOKEN;
+const 1502680320288489563 = process.env.CLIENT_ID;
