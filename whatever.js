cd9cf778db6fdc320d5baa766a42c6a7d01884c1
RadtALIVE <radt.yt.dartt@gmail.com>
2026-05-18 20:47:50 +0530

    Delete whatever.js

--- whatever.js
+++
@@ -1,6 +0,0 @@
-discord-banking-bot/
-├── index.js       ← bot startup & command router
-├── banking.js     ← all commands (already downloaded)
-├── db.js          ← postgresql database functions
-├── package.json   ← libraries list
-└── .gitignore     ← keeps secrets off GitHub
