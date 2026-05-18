f6a1db7637763c6762768b989dc8c0cdeca8c20d
RadtALIVE <radt.yt.dartt@gmail.com>
2026-05-18 20:31:59 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+  "name": "bank-of-hooverton",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "discord.js": "^14.0.0",
+    "@discordjs/rest": "^2.0.0",
+    "discord-api-types": "^0.37.0",
+    "pg": "^8.11.0",
+    "dotenv": "^16.0.0"
+  }
+}
