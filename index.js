3c7c36e0b1b023f5b59bfce3600604f8633fd06c
RadtALIVE <radt.yt.dartt@gmail.com>
2026-05-18 21:12:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v10');
